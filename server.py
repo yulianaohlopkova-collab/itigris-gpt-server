433f12e3661e3cbf9628dfd112c763984e10947e
yulianaohlopkova-collab <yulianaohlopkova@gmail.com>
2026-03-31 11:36:09 +0900

    аминь 2server.py

--- server.py
+++ server.py
@@ -1275,9 +1275,9 @@ async def count_real(
 
     html = await fetch_report_page(payload)
 
-print("HTML RESPONSE START")
-print(html[:1000])
-print("HTML RESPONSE END")
+    print("HTML RESPONSE START")
+    print(html[:1000])
+    print("HTML RESPONSE END")
 
     soup = BeautifulSoup(html, "html.parser")
     text = soup.get_text()
